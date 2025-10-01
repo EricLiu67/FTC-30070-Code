05bb8d39aaaefb5252552a8f6ea8c446a5a589ce
OrreoerrO <107547@scgs.qld.edu.au>
2025-10-01 10:43:51 +1000

    Update Teleop.java

--- Teleop.java
+++ Teleop.java
@@ -7,7 +7,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.rowanmcalpin.nextftc.core.command.Command;
 import com.rowanmcalpin.nextftc.ftc.NextFTCOpMode;
 
+//import core.subsystems.Intake;
 import core.subsystems.Intake;
+import core.subsystems.Outtake;
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
@@ -30,8 +32,15 @@ public class Teleop extends NextFTCOpMode {
         follower.setStartingPose(startPose);
         follower.startTeleopDrive();
 
-        //gamepadManager.getGamepad1().getCircle().setPressedCommand(Intake.INSTANCE::extended);
-        //gamepadManager.getGamepad1().getSquare().setPressedCommand(Intake.INSTANCE::retracted);
+        gamepadManager.getGamepad1().getCircle().setHeldCommand(Intake.INSTANCE::rotatingForward);
+        gamepadManager.getGamepad1().getSquare().setHeldCommand(Intake.INSTANCE::rotatingBackward);
+        gamepadManager.getGamepad1().getTriangle().setPressedCommand(Intake.INSTANCE::bootRotating);
+        gamepadManager.getGamepad1().getX().setPressedCommand(Intake.INSTANCE::bootNotRotating);
+
+        gamepadManager.getGamepad1().getDpadDown().setPressedCommand(Outtake.INSTANCE::OuttakeRotating);
+        gamepadManager.getGamepad1().getDpadDown().setPressedCommand(Outtake.INSTANCE::OuttakeNotRotating);
+
+
 
         follower.setTeleOpMovementVectors(-gamepad1.left_stick_y, gamepad1.left_stick_x, -gamepad1.right_stick_x, true);
         follower.update();
